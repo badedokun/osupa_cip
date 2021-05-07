8a0e06beab2c9da8d4bf18188a5af76465093c26
Jacob Shumate <jacobshumate1999@gmail.com>
2021-05-06 21:37:38 -0500

    Update KYCVerification.js
    All functions complete. Refactoring and revisioning still necessary.

--- KYCVerification.js
+++ KYCVerification.js
@@ -2,29 +2,35 @@ const axios = require('axios');
 const fuzz = require('fuzzball');
 const admin = require('firebase-admin');
 const csv = require('csv-parser')
+const pdf = require('pdf-parse');
 const fs = require('fs')
-const serviceAccount = require('FIRESTORE CREDENTIALS');
+var cron = require('node-cron');
+const https = require('https');
+var crypto = require('crypto');
+const { exec } = require("child_process");
+const serviceAccount = require('./osupa-f56dd-firebase-adminsdk-x4l47-e2a1f979c2.json');
+
 admin.initializeApp({
         credential: admin.credential.cert(serviceAccount)
 });
 const db = admin.firestore()
-//ignores undefined values inserted into firestore
+
 admin.firestore().settings({
         ignoreUndefinedProperties:true,
 })
 
+var pixlabAPIkey;
+
+//Images used for testing
+//srcImage ="https://apscitu.com/Expert-IT-News/Articles/2018/10/08/Insecure_Facebook_Demands_Your_Passport_Bank_Statements_Medical_Records/FakeMarkZuckerbergPassport_488x324.png"
+//targetImage="https://s3.amazonaws.com/pics.pixlab.xyz/BblNFPTCEAAvPZL.jpg"
 
 
-srcImage =""
-targetImage=""
-
-//Compares uploaded selfie to parse photo from ID, uses image links as input
 async function faceCompare(srcImage,targetImage){
         try {
-                //sends images to pixlab facecompare endpoint and returns the field same_face which is a numeric confidence score
                 const response = await axios.get('https://api.pixlab.io/facecompare',{
                         params:{
-                                'key':'#################',
+                                'key':pixlabAPIkey,
                                 'src':srcImage,
                                 'target':targetImage
                         }
@@ -36,13 +42,11 @@ async function faceCompare(srcImage,targetImage){
         }
 
 }
-//Parses text from ID Card
 async function IDParsing(img,country){
         try {
-                //Returns data parsed from ID
                 const response = await axios.get('https://api.pixlab.io/docscan',{
                         params:{
-                                'key':'##################',
+                                'key':pixlabAPIkey,
                                 'img':img,
                                 'type':'idcard',
                                 'country':country
@@ -55,14 +59,11 @@ async function IDParsing(img,country){
         }
 }
 
-
-//Parses text from a passport
 async function PassportParse(img){
         try {
-                //Returns data parsed from passport
                 const response = await axios.get('https://api.pixlab.io/docscan',{
                         params:{
-                                'key':'##################',
+                                'key':pixlabAPIkey,
                                 'img':img,
                                 'type':'passport',
                         }
@@ -75,21 +76,15 @@ async function PassportParse(img){
 }
 
 
-async function dataCompare(userInputJSON, IDparseJSON){
-//Going to compare two json objects similarity 
-}
 
-//Compares name and address parsed from ID to data inputted into firestore
 async function dataCompare(ufirstName,umiddleName,ulastName,IDParseJSON){
         var dataValidation;
-        //firestore query
         await db.collection('users').where('firstName','==',ufirstName).where('middleName','==',umiddleName).where('lastName','==',ulastName).get().then((snapshot) => {
                 snapshot.docs.forEach(doc => {
-                        //fuzz ratio is numeric score that indicates string similarity, if the strings have a fuzz ratio more than 90, it is safe to assume they are the same
                         name_fuzz_ratio=fuzz.ratio(ufirstName+umiddleName+ulastName , IDParseJSON['name']);
                         address_fuzz_ratio=fuzz.ratio(doc.data().address1 , IDParseJSON["address"]);
+                        //dob_fuzz_ratio
                         if(name_fuzz_ratio >= 90 && address_fuzz_ratio >= 90){
-                            //returns true if the data is the same
                                 dataValidation = true;
                         }
                         else{
@@ -102,18 +97,31 @@ async function dataCompare(ufirstName,umiddleName,ulastName,IDParseJSON){
 }
 
 
-//compares string parsed from a id's mrz and compares it to the user inputted name
+
 async function MRZCompare(nameString,MRZString,){
         return(MRZString.includes(nameString) ? true : false);
 }
 
 
-//Takes name as input and checks to see if it exists in OFAC'S SDN list
-async function ofacScreening(nameInput){
+
+async function PEPScreening(firstName,lastName){
+        var nameFound = false;
+        //const query = await db.collection('CIA PEP').where('LASTNAME','==',nameInput).get();
+        const query = await db.collection('CIA PEP').where('FIRSTNAME','>=',firstName).where("LASTNAME", '>=', lastName).get();
+        query.forEach(doc => {
+            if(doc.data()['FIRSTNAME'].length > 1){
+                nameFound = true;
+            }
+        })
+        return nameFound;
+}
+
+async function ofacScreening(firstName,lastName){
+    //fullNameArray=data['NAME'].split(",");
     var nameFound = false;
-    const query = await db.collection('OFAC SDN').where('NAME','==',nameInput).get();
+    const query = await db.collection('OFAC SDN').where('FIRSTNAME','>=',firstName).where("LASTNAME", '>=', lastName).get();
     query.forEach(doc => {
-        if(doc.data()['NAME'].length > 1){
+        if(doc.data()['FIRSTNAME'].length > 1){
             nameFound = true;
         }
     })
@@ -121,20 +129,53 @@ async function ofacScreening(nameInput){
 }
 
 
-//var sdnJSON = require('./sdn.json');
 
-//Parses csv data from csv file(OFAC SDN CSV FILE) and enumerates through every entry, if the entry doesn't exist yet and it's not undefined, it inserts it into firestore.
+async function PEPupdate(PEPfile){
+        var lastName;
+        let dataBuffer = fs.readFileSync(PEPfile);
+        pdf(dataBuffer).then(function(data) {
+                PEPArray=data.text.split("\n");
+                PEPArray.forEach(element =>{
+                        splitCases=element.split(/(\b[A-Z][A-Z]+|\b[A-Z]\b)/g);
+                        console.log(splitCases);
+                        if(splitCases.length > 2){
+                                //combines remaining elements of array if uppercase portion(last name) is more than one word
+                                combineUppercase=splitCases.slice(1,splitCases.length).join();
+                                lastName=combineUppercase.replace(/\,/g,"");
+                        }
+                        //if(!(element.includes("Min.")  || element.includes("Pres.") || element.includes("Sec.")  || element.includes("Dep."))){
+                                
+                        const res = db.collection('CIA PEP').add({
+                                FIRSTNAME:splitCases[0],
+                                LASTNAME:lastName
+                        });
+                });
+                
+            });
+}
+
+//var sdnJSON = require('./sdn.json');
+const { Console } = require('console');
 async function ofacUpdate(csvFile){
     var readStream =fs.createReadStream(csvFile)
     .pipe(csv())
     .on('data', async (data) => {
-        var existenceCheck = await ofacScreening(data['NAME']);
-        if(existenceCheck != true && data['NAME'] != undefined){
-                const res = db.collection('OFAC SDN').add({
-                        NAME:data['NAME'],
-                        SDNTYPE:data['SDNTYPE'],
-                        PROGRAM:data['PROGRAM']
-                });
+        try{
+                fullNameArray=data['NAME'].split(",");
+                var existenceCheck = await ofacScreening(fullNameArray[0],fullNameArray[1]);
+                if(existenceCheck != true && data['NAME'] != undefined){
+                        if(data['SDNTYPE']=="individual"){
+                                const res = db.collection('OFAC SDN').add({
+                                        LASTNAME:fullNameArray[0],
+                                        FIRSTNAME:fullNameArray[1],
+                                        SDNTYPE:data['SDNTYPE'],
+                                        PROGRAM:data['PROGRAM']
+                                });
+                        }
+                }
+        }
+        catch(err){
+                console.log(err);
         }
     })
     .on('error', function(error){
@@ -145,17 +186,86 @@ async function ofacUpdate(csvFile){
     })
 }
 
+/*
+async function ofacDeleteThenUpdate(){
+        db.collection("OFAC SDN").get().then(function(querySnapshot) {
+                querySnapshot.forEach( async function(doc) {
+                        const res = await db.collection('OFAC SDN').doc(doc).delete();
+                    
+                });
+            });
+}
+*/
 
-async function ofacCheckForUpdate(){
-//The plan here is to use nodecron to frequently download the sdn.csv file, check the hash, and if it's different, use sed and awk to insert headers into the file(FOR THE CSV PARSER) and insert it into ofacUpdate
+
+async function getSdnHash(){
+        var firestoreHash;
+        const firestoreHashQuery = await db.collection('KYCTesting').where('sdnFileHash','!=', false).get();
+        firestoreHashQuery.forEach(doc => {
+                firestoreHash=doc.data()["sdnFileHash"];
+        })
+        return firestoreHash;
 }
 
-async function IDVerification(){
-//The plan here is to use this function to contact an authoritative source for the ID(Such as DMV Database) to verify that it's not counterfeit
+
+cron.schedule('30 3 15 * *', async () => {
+        var firestoreHash = await getSdnHash();
+        const SDNurl = "https://www.treasury.gov/ofac/downloads/sdn.csv";    
+        var fileHash;
+        const path = `/tmp/sdn.csv`; 
+        https.get(SDNurl,(res) => {
+                const filePath = fs.createWriteStream(path);
+                res.pipe(filePath);
+                filePath.on('finish',() => {
+                    filePath.close();
+                    fs.createReadStream(path).
+                    pipe(crypto.createHash('sha256').setEncoding('hex')).
+                    on('finish', async function () {
+                       fileHash=this.read();
+                       if(fileHash != firestoreHash){
+                                const data = {sdnFileHash:fileHash};
+                                console.log(data);
+                                const res = await db.collection('KYCTesting').doc('sdnSHA256').set(data);
+                                exec("sed -i -e '1iID,NAME,SDNTYPE,PROGRAM\' "+path, (error, stdout, stderr) => {
+                                        if (error) {
+                                            console.log(`error: ${error.message}`);
+                                            return;
+                                        }
+                                        if (stderr) {
+                                            console.log(`stderr: ${stderr}`);
+                                            return;
+                                        }
+                                        console.log(`stdout: ${stdout}`);
+                                    });
+                                await ofacUpdate(path);
+                                fs.unlinkSync(path);
+                       }
+                       else{
+                               console.log("hash is same")
+                                fs.unlinkSync(path);
+                       }
+                    })
+                })
+            })
+});
+
+
+
+
+async function setPixlabApiKey(){
+    const apiKeyQuery = await db.collection('KYCTesting').where('PIXLABAPIKEY','!=', false).get();
+    apiKeyQuery.forEach(doc => {
+            pixlabAPIkey=doc.data();
+    })
 }
 
 async function KYCVerification(){
-//This is the primary function that will consolidate all the other functions as verification checks to eventually enable the KYC flag if it passes, otherwise not.
+
+    //ofacCheckForUpdate();
+    //ofacUpdate("newsdn.csv");
+    //PEPupdate('PEP.pdf');
+    //b= await PEPScreening("KULFAS");
+    //console.log(b);
 }
 
 
